86837ad1c42d11351c490b6fb9ce38e04e2c9fbe
Ali Demirtaş <alidemirtas.7@icloud.com>
2022-08-14 22:01:55 +0300

    onboardin pages is ok.

--- Bankey/Bankey/Onboarding/OnboardingViewController.swift
+++ Bankey/Bankey/Onboarding/OnboardingViewController.swift
@@ -13,6 +13,9 @@ class OnboardingViewController: UIViewController {
     let imageView = UIImageView()
     let label = UILabel()
     
+    let heroImageName: String
+    let titleText: String
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,6 +23,16 @@ class OnboardingViewController: UIViewController {
         layout()
     }
     
+    init(heroImageName:String, titleText:String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 extension OnboardingViewController {
     func style() {
@@ -31,7 +44,7 @@ extension OnboardingViewController {
         
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "delorean")
+        imageView.image = UIImage(named: heroImageName)
         
         // Label
         
@@ -41,9 +54,10 @@ extension OnboardingViewController {
         label.adjustsFontForContentSizeCategory = true
         label.numberOfLines = 0
         label.textAlignment = .center
-        label.text = "Bankey is faster, easier to use and has a brand new look and feel that will make you feel like you are back in 1989."
+        label.text = titleText
     }
     func layout() {
+        view.backgroundColor = .systemBackground
         stackView.addArrangedSubview(imageView)
         stackView.addArrangedSubview(label)
         view.addSubview(stackView)
